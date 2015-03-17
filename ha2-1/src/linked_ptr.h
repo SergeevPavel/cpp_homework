bcdeaed8a56ac0c6d7eeb4a1e6b44250f96323a4
pavel <sergeev.pavel.al@gmail.com>
2015-03-17 23:42:36 +0300

    not working version with cyclic list

--- ha2-1/src/linked_ptr.h
+++ ha2-1/src/linked_ptr.h
@@ -1,87 +1,106 @@
 #ifndef LINKED_PTR_H
 #define LINKED_PTR_H
 
+#include <utility>
+
 namespace smart_ptr
 {
 
 template <class T>
-class smart_ptr
+class linked_ptr
 {
 public:
-    smart_ptr()
-        : pointee_(nullptr)
-        , left(this)
-        , right(this)
+    linked_ptr()
+        : node(&node, &node)
+        , pointee_(nullptr)
     {
     }
 
-    explicit smart_ptr(T* pointee)
-        : pointee_(pointee)
-        , left(this)
-        , right(this)
-    {
+//    explicit linked_ptr(T* pointee)
+//        : pointee_(pointee)
+//        , left_(this)
+//        , right_(this)
+//    {
+//    }
 
+    template <class U>
+    explicit linked_ptr(U* pointee)
+        : node(&node, &node)
+        , pointee_(pointee)
+    {
     }
 
-    smart_ptr(const smart_ptr<T>& other)
+    linked_ptr(const linked_ptr& other)
+        : node(&other.node, other.node.right_)
+        , pointee_(other.pointee_)
     {
-        // TODO
+        // check selfcopy
+        insert_me();
     }
 
     template <class U>
-    explicit smart_ptr(U* pointee)
-        : pointee_(pointee)
-        , left(this)
-        , right(this)
+    linked_ptr(const linked_ptr<U>& other)
+        : node(&other.node, other.node.right_)
+        , pointee_(other.pointee_)
     {
+        insert_me();
+    }
+
+    linked_ptr& operator= (linked_ptr other)
+    {
+        swap(other);
+        return *this;
     }
 
     template <class U>
-    smart_ptr(const smart_ptr<U>& other)
+    linked_ptr& operator= (const linked_ptr<U>& other)
     {
-        // TODO
-    }
-
-    smart_ptr& operator= (const smart_ptr<T>& other)
-    {
-        // TODO
-    }
-
-    template <class U>
-    smart_ptr& operator= (const smart_ptr<U>& other)
-    {
-        // TODO
+        linked_ptr temp(other);
+        swap(temp);
+        return *this;
     }
 
     void reset()
     {
-        // TODO
+//        pointee_ = nullptr;
+//        drop_me();
+//        node.left_ = &node;
+//        node.right_ = &node;
+        linked_ptr temp;
+        swap(temp);
     }
 
-    void reset(T* pointee)
-    {
-        // TODO
-    }
+//    void reset(T* pointee)
+//    {
+//        // TODO
+//    }
 
     template<class U>
     void reset(U* pointee)
     {
-        // TODO
+        linked_ptr temp(pointee);
+        swap(temp);
     }
 
-    void swap(smart_ptr& other)
+    void swap(linked_ptr& other)
     {
-        // TODO
+        std::swap(pointee_, other.pointee_);
+        if (!is_neighboring(other) && !is_one_in_list())
+        {
+            std::swap(node, other.node);
+            insert_me();
+            other.insert_me();
+        }
     }
 
     T* get() const
     {
-        // TODO
+        return pointee_;
     }
 
     bool unique() const
     {
-        // TODO
+        return is_one_in_list() && (pointee_ != nullptr);
     }
 
     T* operator-> () const
@@ -95,19 +114,101 @@ public:
         return *pointee_;
     }
 
-    // add comapasion operators
-
     explicit operator bool () const
     {
         return pointee_ != nullptr;
     }
+
+    ~linked_ptr()
+    {
+        enum {T_IS_INCOMPLETE_TYPE = sizeof(T)};
+        if (is_one_in_list())
+        {
+            delete pointee_;
+        }
+        else
+        {
+            pointee_ = nullptr;
+            drop_me();
+        }
+    }
+
 private:
+
+    struct node_t
+    {
+        node_t(node_t const* left, node_t const* right)
+            : left_(left)
+            , right_(right)
+        {
+        }
+
+        mutable node_t const* left_;
+        mutable node_t const* right_;
+    } node;
+
+    void insert_me()
+    {
+        node.left_->right_ = &node;
+        node.right_->left_ = &node;
+    }
+
+    void drop_me()
+    {
+        node.right_->left_ = node.left_;
+        node.left_->right_ = node.right_;
+    }
+
+    bool is_neighboring(const linked_ptr& other) const
+    {
+        return node.right_ == &other.node || node.left_ == &other.node;
+    }
+
+    bool is_one_in_list() const
+    {
+        return node.right_ == &node;
+    }
+
     T* pointee_;
-    smart_ptr* left;
-    smart_ptr* right;
 };
 
-} // smart_ptr
+template <class T, class U>
+bool operator == (linked_ptr<T> left, linked_ptr<U> right)
+{
+    return left.get() == right.get();
+}
+
+template <class T, class U>
+bool operator != (linked_ptr<T> left, linked_ptr<U> right)
+{
+    return left.get() != right.get();
+}
+
+template <class T, class U>
+bool operator < (linked_ptr<T> left, linked_ptr<U> right)
+{
+    return left.get() < right.get();
+}
+
+template <class T, class U>
+bool operator <= (linked_ptr<T> left, linked_ptr<U> right)
+{
+    return left.get() <= right.get();
+}
+
+template <class T, class U>
+bool operator > (linked_ptr<T> left, linked_ptr<U> right)
+{
+    return left.get() > right.get();
+}
+
+template <class T, class U>
+bool operator >= (linked_ptr<T> left, linked_ptr<U> right)
+{
+    return left.get() >= right.get();
+}
+
+} // linked_ptr
 
 #endif // LINKED_PTR_H
 
--- ha2-1/src/main.cpp
+++ ha2-1/src/main.cpp
@@ -2,112 +2,133 @@
 #include <cassert>
 #include "linked_ptr.h"
 
+#include <memory>
+
 using namespace smart_ptr;
 
-//static void test_empty_lptr()
-//{
-//    linked_ptr<int> lpi_empty;
-//    assert(!lpi_empty);
-//    lpi_empty.reset();
-//    assert(!lpi_empty);
-//    assert(!lpi_empty.get());
-//    assert(!lpi_empty.unique());
-//}
+struct Base
+{
+    virtual bool is_base() { return true; }
+    virtual ~Base() {}
+};
+struct Derived : Base
+{
+    virtual bool is_base() override { return false; }
+};
 
-//static void test_one_lptr()
-//{
-//    linked_ptr<int> lpi_single(new int(777));
-//    assert(lpi_single);
-//    lpi_single.reset(new int(666));
-//    assert(lpi_single);
-//    assert(lpi_single.get());
-//    assert(lpi_single.unique());
-//}
+static void test_empty_lptr()
+{
+    linked_ptr<int> lpi_empty;
+    assert(!lpi_empty);
+    lpi_empty.reset();
+    assert(!lpi_empty);
+    assert(!lpi_empty.get());
+    assert(!lpi_empty.unique());
+}
 
-//static void test_simple_assignment()
-//{
-//    using namespace std;
-//    linked_ptr<int> ptr1(new int(5));
-//    linked_ptr<int> ptr2(new int(6));
-//    assert(*ptr1 == 5);
-//    assert(*ptr2 == 6);
-//    ptr1 = ptr2;
-//    assert(*ptr1 == 6);
-//    assert(*ptr2 == 6);
-//}
+static void test_one_lptr()
+{
+    linked_ptr<int> lpi_single(new int(777));
+    assert(lpi_single);
+    lpi_single.reset(new int(666));
+    assert(lpi_single);
+    assert(lpi_single.get());
+    assert(lpi_single.unique());
+}
 
-//static void test_many_lptr()
-//{
-//    linked_ptr<int> lp;
-//    lp = linked_ptr<int>(new int(777));
-//    auto lp1(lp);
-//    auto lp2(lp1);
-//    auto lp3(lp2);
-//    auto lp4(lp);
-//    auto lp5(lp1);
-//    auto lp6(lp1);
-//    auto lp7(lp3);
-//    auto lp8(lp7);
-//    auto lp9(lp5);
-//    lp7.reset();
-//    auto lp10(lp7);
-//    auto lp11(lp6);
-//    auto lp12(lp1);
-//    auto lp13(lp2);
-//    lp13.reset(new int(666));
-//    auto lp14(lp3);
-//    auto lp15(lp13);
-//    auto lp16(lp12);
-//    auto lp17(lp15);
-//}
+static void test_simple_assignment()
+{
+    using namespace std;
+    linked_ptr<int> ptr1(new int(5));
+    linked_ptr<int> ptr2(new int(6));
+    assert(*ptr1 == 5);
+    assert(*ptr2 == 6);
+    ptr1 = ptr2;
+    assert(*ptr1 == 6);
+    assert(*ptr2 == 6);
+}
 
-//static void test_lptr_conversions()
-//{
+static void test_many_lptr()
+{
+    linked_ptr<int> lp;
+    lp = linked_ptr<int>(new int(777));
+    auto lp1(lp);
+    auto lp2(lp1);
+    auto lp3(lp2);
+    auto lp4(lp);
+    auto lp5(lp1);
+    auto lp6(lp1);
+    auto lp7(lp3);
+    auto lp8(lp7);
+    auto lp9(lp5);
+    lp7.reset();
+    auto lp10(lp7);
+    auto lp11(lp6);
+    auto lp12(lp1);
+    auto lp13(lp2);
+    lp13.reset(new int(666));
+    auto lp14(lp3);
+    auto lp15(lp13);
+    auto lp16(lp12);
+    auto lp17(lp15);
+}
 
-//    struct base
-//    {
-//        virtual bool is_base() { return true; }
-//        virtual ~base() {}
-//    };
-//    struct derived : base
-//    {
-//        virtual bool is_base() { return false; }
-//    };
-//    linked_ptr<base> lptr(new base());
-//    assert(lptr->is_base());
-//    lptr = linked_ptr<base>(new derived());
-//    assert(!lptr->is_base());
-//}
+static void test_lptr_conversions()
+{
 
-//static void test_lptr_delete()
-//{
-//    struct foo;
-//    linked_ptr<foo> lptr();
-//    struct foo {}; //compiler error if not defined here
-//}
+    struct base
+    {
+        virtual bool is_base() { return true; }
+        virtual ~base() {}
+    };
+    struct derived : base
+    {
+        virtual bool is_base() { return false; }
+    };
+    linked_ptr<base> lptr(new base());
+    assert(lptr->is_base());
+    lptr = linked_ptr<base>(new derived());
+    assert(!lptr->is_base());
+}
+
+static void test_lptr_delete()
+{
+    struct foo;
+    linked_ptr<foo> lptr;
+    struct foo {}; //compiler error if not defined here
+}
 
 static void test_constructors()
 {
-    struct Base
-    {
-        virtual bool is_base() { return true; }
-        virtual ~Base() {}
-    };
-    struct Derived : Base
-    {
-        virtual bool is_base() override { return false; }
-    };
+    linked_ptr<int> first;
+    linked_ptr<int> second(first);
+}
 
+void test_swap()
+{
+    linked_ptr<int> lp1(new int);
+    linked_ptr<int> lp2(lp1);
+    linked_ptr<int> lp3(lp2);
+    linked_ptr<int> lp4(lp3);
+
+    lp1.swap(lp3); // swap non neighboring elements
+    lp2.swap(lp3); // swap neighboring elements
+    lp1.swap(lp1); // selfswap
 }
 
 int main()
 {
+//my tests
+    test_swap();
+//    test_non_complete_type();
+
+//other tests
     test_constructors();
-//    test_empty_lptr();
-//    test_one_lptr();
-//    test_simple_assignment();
-//    test_many_lptr();
-//    test_lptr_conversions();
-//    test_lptr_delete();
+    test_empty_lptr();
+    test_one_lptr();
+    test_simple_assignment();
+    test_many_lptr();
+    test_lptr_conversions();
+    test_lptr_delete();
     return 0;
 }
